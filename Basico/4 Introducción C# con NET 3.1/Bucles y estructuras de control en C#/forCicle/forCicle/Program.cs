3d6222df60274969fcace55d3bc5e843a94f2094
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-01-16 12:30:46 -0800

    21: forCicle

---
+++ Basico/4 Introducción C# con NET 3.1/Bucles y estructuras de control en C#/forCicle/forCicle/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Outer loop
+for (int i = 1; i <= 2; ++i)
+{
+    Console.WriteLine("Outer: " + i);  // Executes 2 times
+
+    // Inner loop
+    for (int j = 1; j <= 3; j++)
+    {
+        Console.WriteLine(" Inner: " + j); // Executes 6 times (2 * 3)
+    }
+}
