f58d836007cc1a0acbb2578d0d85210cf04d6c8b
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2023-01-16 12:07:19 -0800

    20: Sentencia Switch

---
+++ Basico/4 Introducción C# con NET 3.1/Bucles y estructuras de control en C#/SentenciaSwitch/SentenciaSwitch/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter de selected soda: ");
+string caseSwitch = Console.ReadLine();
+
+switch (caseSwitch)
+{
+    case "cola":
+        Console.WriteLine("Cola soda - $2 US Dolar");
+        break;
+    case "lime":
+        Console.WriteLine("Lime soda - $2 US Dolar");
+        break;
+    case "orange":
+        Console.WriteLine("Orange soda - $2 US Dolar");
+        break;
+    case "apple":
+        Console.WriteLine("Apple soda - $2 US Dolar");
+        break;
+    case "fanta":
+        Console.WriteLine("Fanta soda - $2 US Dolar");
+        break;
+    default:
+        Console.WriteLine("ERROR: You did not select a soda or you entered an incorrect value.");
+        break;
+}
+
+
