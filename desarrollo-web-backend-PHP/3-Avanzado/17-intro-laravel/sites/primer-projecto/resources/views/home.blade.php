4e139fcfca46e5afe23f114251d03abe6ba6a583
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-02-28 16:36:18 -0600

    Update

---
+++ desarrollo-web-backend-PHP/3-Avanzado/17-intro-laravel/sites/primer-projecto/resources/views/home.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello, World!</title>
+	<style>
+		body {
+			background-color: #f0f0f0;
+			font-family: Arial, sans-serif;
+			text-align: center;
+			margin-top: 100px;
+		}
+		
+		h1 {
+			font-size: 48px;
+			color: #333;
+			margin-bottom: 20px;
+		}
+		
+		p {
+			font-size: 24px;
+			color: #666;
+			margin-bottom: 40px;
+		}
+	</style>
+</head>
+<body>
+	<h1>Hello, World!</h1>
+	<p>Welcome to my first web page.</p>
+</body>
+</html>
